e92f3085086b52570993c017f9dabc8fa0e47830
martinashraf01 <martin619100@gmail.com>
2025-02-13 15:53:11 +0200

    disable cors

--- src/main/java/taskaya/backend/config/security/SecurityConfig.java
+++ src/main/java/taskaya/backend/config/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
