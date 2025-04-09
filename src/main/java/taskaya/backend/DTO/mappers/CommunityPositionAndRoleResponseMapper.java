52e2279b3cd726ff48b7371f74276a890e56fafb
Mina Hany <minahany910@gmail.com>
2025-04-09 02:19:18 +0200

    edit unassinged community members

--- src/main/java/taskaya/backend/DTO/mappers/CommunityPositionAndRoleResponseMapper.java
+++ src/main/java/taskaya/backend/DTO/mappers/CommunityPositionAndRoleResponseMapper.java
@@ -1,7 +1,9 @@
 package taskaya.backend.DTO.mappers;
 
 import org.springframework.stereotype.Component;
+import taskaya.backend.DTO.communities.communityMember.responses.CommunityMemberCommunityProfileDTO;
 import taskaya.backend.DTO.communities.communityMember.responses.CommunityMemberSettingsResponseDTO;
+import taskaya.backend.config.Constants;
 import taskaya.backend.entity.community.CommunityMember;
 
 import java.util.List;
@@ -23,6 +25,14 @@ public class CommunityPositionAndRoleResponseMapper {
                 .build();
         if(communityMember.getFreelancer() != null)
             memberDTO.setNameAndPicture(CommunityMemberCommunityProfileResponseMapper.toDTO(communityMember));
+        else {
+            memberDTO.setNameAndPicture(CommunityMemberCommunityProfileDTO.builder()
+                            .name("Unassinged")
+                            .freelancerProfilePicture(Constants.FIRST_PROFILE_PICTURE)
+                            .position(communityMember.getPositionName())
+                    .build());
+        }
+
         return memberDTO;
     }
 
