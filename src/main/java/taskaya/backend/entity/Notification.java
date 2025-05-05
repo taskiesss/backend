4fd5e74c6d2ab776fb7fee3383ab7973f42e1734
martinashraf01 <martin619100@gmail.com>
2025-05-05 16:05:15 +0300

    add notification entity

---
+++ src/main/java/taskaya/backend/entity/Notification.java
@@ -0,0 +1,39 @@
+package taskaya.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import taskaya.backend.entity.enums.NotificationDest;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "notifications")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id; // Primary key
+
+    @Column(name = "title", nullable = false, length = 255)
+    String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User user;
+    @Column(name = "is_read", nullable = false)
+    @Builder.Default
+    boolean isRead= false;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    NotificationDest type;
+
+    @Column(name = "route_id", nullable = false)
+    String routeId;
+}
---
+++ src/main/java/taskaya/backend/entity/enums/NotificationDest.java
@@ -0,0 +1,17 @@
+package taskaya.backend.entity.enums;
+
+public enum NotificationDest {
+
+    PROPOSAL, //{send prop to client ,acceptance , rejection}
+
+    CONTRACT, //{new offer for freelancers , acceptance(client and freelancer) , rejection(client) ,milestone review req to client ,send milestone approval to freelancer  }
+
+    COMMUNITY_PROFILE, //{to freelancer after join acceptance }
+
+    COMMUNITY_JOBS_AND_TALENTS, //{if there is a new join request (appears for community admin only)}
+
+    COMMUNITY_POSTS, //{new post , comments }
+
+    COMMUNITY_SETTINGS, //{new settings (for all members )}
+
+}
